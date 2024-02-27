0ca4837e9ebdc26b171c7de6efa5874c2779cf40
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-02-27 06:52:28 +0000

    "ok"

---
+++ week-2/Pythagoras.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class Pythagoras {
+    public static void main(String[] args) {
+
+    double a = 3.0;
+    double b = 5.0;
+    double c = (Math.sqrt(a * a + b * b));
+
+    System.out.println(c);
+    }
+}
+    
--- week-2/SimpleCircle.java
+++ week-2/SimpleCircle.java
@@ -4,8 +4,8 @@ public class SimpleCircle {
     public static void main (String args[]) {
         double r = 2.5;
         // replace the following two lines
-        double area = (25/4 * Math.PI)
-        double circumference = (5 * Math.PI)
+        double area = (25/4 * Math.PI);
+        double circumference = (5 * Math.PI);
         System.out.println(r);
         System.out.println(area);
         System.out.println(circumference);
