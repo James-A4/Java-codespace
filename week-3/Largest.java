2d485d4055d931395967ee5366443482d3a49c8d
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-03-01 19:43:34 +0000

    "Largest java"

--- week-3/Largest.java
+++ week-3/Largest.java
@@ -1,5 +1,22 @@
-week-3/Largest,java
+public class Largest {
+    public static void main(String[] args) {
+        int n = Comp122.getInt("Length of Array:");
+        int[] myArray = new int[n];
 
-week-3/Largest,java
+        for (int i = 0;i < n; i++) {
+            myArray[i] = Comp122.getInt("Enter an integer:");
+        }
 
-week-3/Largest,java
+        if (n > 0) {
+        int largestValue = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (myArray[i] > largestValue) {
+                largestValue = myArray[i];
+            }
+        }
+
+        System.out.println(largestValue);
+        }
+    }
+}
