aa773f2af006feddc719cdbf93bfd06f5b45f98c
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-03-26 03:24:10 +0000

    "Getting going"

--- week-8/MonoAlphaSubstitution.java
+++ week-8/MonoAlphaSubstitution.java
@@ -1,5 +1,21 @@
 public class MonoAlphaSubstitution extends Substitution {
 
+
+    public static char OriginalLetters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                                            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                                            'u', 'v', 'w', 'x', 'y', 'z'};
+    
+
+    public static char MappedLetters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                                          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                                          'u', 'v', 'w', 'x', 'y', 'z'};
+
+
+
+
+
+
+
     public char encrypt(char ch) {
         ////
         ////
