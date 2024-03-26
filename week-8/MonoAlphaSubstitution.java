424a83232c1af8d720c90f075b190d0da0a7ce49
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-03-26 02:22:15 +0000

    "Co-pilot attempt"

--- week-8/MonoAlphaSubstitution.java
+++ week-8/MonoAlphaSubstitution.java
@@ -1,3 +1,51 @@
-public class MonoAlphaSubstitution {
-    
+public class MonoAlphaSubstitution extends Substitution {
+    private String translationTable;
+
+    public MonoAlphaSubstitution(String translationTable) {
+        this.translationTable = translationTable;
+    }
+
+    @Override
+    public char encrypt(char c) {
+        int index = translationTable.indexOf(c);
+        if (index != -1) {
+            return translationTable.charAt(index);
+        }
+        return c;
+    }
+
+    @Override
+    public char decrypt(char c) {
+        int index = translationTable.indexOf(c);
+        if (index != -1) {
+            return (char) ('A' + index);
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java MonoAlphaSubstitution encrypt|decrypt key \"cipher text\"");
+            return;
+        }
+
+        String direction = args[0];
+        String key = args[1].toUpperCase();
+        String text = args[2].toUpperCase();
+
+        MonoAlphaSubstitution cipher = new MonoAlphaSubstitution(key);
+
+        if (direction.equals("encrypt")) {
+            for (char c : text.toCharArray()) {
+                System.out.print(cipher.encrypt(c));
+            }
+        } else if (direction.equals("decrypt")) {
+            for (char c : text.toCharArray()) {
+                System.out.print(cipher.decrypt(c));
+            }
+        } else {
+            System.out.println("The first parameter must be \"encrypt\" or \"decrypt\"!");
+            System.out.println("Usage: java MonoAlphaSubstitution encrypt|decrypt key \"cipher text\"");
+        }
+    }
 }
--- week-8/Vigenere.java
+++ week-8/Vigenere.java
@@ -1,3 +1,68 @@
+import java.util.Arrays;
+
 public class Vigenere {
     
+}
+public class Vigenere extends Substitution {
+    private String keyword;
+    private int position;
+
+    public Vigenere() {
+        this.keyword = "";
+        this.position = 0;
+    }
+
+    public Vigenere(String keyword) {
+        this.keyword = keyword;
+        this.position = 0;
+    }
+
+    @Override
+    public char encrypt(char c) {
+        if (!Character.isLetter(c)) {
+            return c;
+        }
+
+        int shift = keyword.charAt(position) - 'A';
+        char encryptedChar = super.encrypt(c, shift);
+        position = (position + 1) % keyword.length();
+        return encryptedChar;
+    }
+
+    @Override
+    public char decrypt(char c) {
+        if (!Character.isLetter(c)) {
+            return c;
+        }
+
+        int shift = keyword.charAt(position) - 'A';
+        char decryptedChar = super.decrypt(c, shift);
+        position = (position + 1) % keyword.length();
+        return decryptedChar;
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Too few parameters!");
+            System.out.println("Usage: java Vigenere encrypt key \"cipher text\"");
+            return;
+        }
+
+        String mode = args[0];
+        String keyword = args[1];
+        String text = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+
+        Vigenere vigenere = new Vigenere(keyword);
+
+        if (mode.equals("encrypt")) {
+            String encryptedText = vigenere.encrypt(text);
+            System.out.println(encryptedText);
+        } else if (mode.equals("decrypt")) {
+            String decryptedText = vigenere.decrypt(text);
+            System.out.println(decryptedText);
+        } else {
+            System.out.println("The first parameter must be \"encrypt\" or \"decrypt\"!");
+            System.out.println("Usage: java Vigenere encrypt key \"cipher text\"");
+        }
+    }
 }
